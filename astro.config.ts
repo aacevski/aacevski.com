2832fd62c994ec4a2f181e76d46b4455af18ad58
Andrej Acevski <andrejacevski@pantone.com>
2024-12-26 22:56:38 +0100

    adding adapter

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  site: 'https://aacevski.com',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  output: 'server',
+  integrations: [sitemap(), tailwind(), mdx()]
+});
