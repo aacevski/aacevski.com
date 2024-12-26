fe4e560f6946b333d908203021f1f8911bc2aa51
Andrej Acevski <andrejacevski@pantone.com>
2024-12-26 22:40:26 +0100

    initial

---
+++ src/config/navigation.ts
@@ -0,0 +1,10 @@
+export const navItems = [
+  { label: "Writing", href: "/blog" },
+  { label: "About", href: "/about" },
+] as const;
+
+export const socials = [
+  { platform: "twitter", url: "https://twitter.com/aacevski" },
+  { platform: "youtube", url: "https://youtube.com/@aacevski1" },
+  { platform: "github", url: "https://github.com/aacevski" },
+] as const;
