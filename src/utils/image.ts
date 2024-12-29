3c23456443f16803cb85290f36b8dd25b8fb6982
Andrej <aacevski@gmail.com>
2024-12-29 14:30:47 +0100

    improving cls

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 export function loadAvatarImage() {
       const image = readFileSync(
-        join(process.cwd(), 'public', 'andrej.png')
+        join(process.cwd(), 'src', 'assets', 'andrej.png')
       );
 
       return `data:image/png;base64,${image.toString('base64')}`;
